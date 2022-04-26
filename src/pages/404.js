a939a4b1a4c8d32f4a53fceed247e3c17bfdd2c6
Yernar Yergaziyev <54137699+yernar707@users.noreply.github.com>
2022-04-26 20:45:45 +0600

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,8 @@
 import * as React from "react"
 
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "@popperjs/core/dist/umd/popper.min.js";
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
