63bee62bcf4ba843d71aa50937613cafd49701cc
Yernar Yergaziyev <54137699+yernar707@users.noreply.github.com>
2022-04-26 20:34:05 +0600

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,18 @@
 import * as React from "react"
 
+import 'bootstrap/dist/css/bootstrap.css';
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const isBrowser = () => typeof window !== "undefined"
 isBrowser()
-
 const NotFoundPage = () => (
   React.useEffect(() => {
-   if (typeof window === "undefined") {
-     console.log(`bailing out of the useeffect. Going to continue to render??`)
-     return
-   }
+   
+  if (typeof window !== 'undefined') {
+      window.jQuery = window.$ = require('jquery');
+      require('bootstrap');
+  }
  },
   <Layout>
     <Seo title="404: Not found" />
