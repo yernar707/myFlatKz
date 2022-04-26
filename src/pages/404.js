98d9934d4208cf633761446eed721b62209e43f9
Yernar Yergaziyev <54137699+yernar707@users.noreply.github.com>
2022-04-26 19:40:05 +0600

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,22 +4,17 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const NotFoundPage = () => (
-   useEffect(() => {
-    if (typeof window === "undefined" || !window.document) {
-      console.log(`bailing out of the useeffect. Going to continue to render??`)
-      return
-    }
-  }, [])
-   return (
-    <Layout>
-      <div class="parent">
-         <Seo title="404: Not found" />
-         <h1>404: Not Found</h1>
-         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </div>
-    </Layout>
-   )
-   
-)
+  React.useEffect(() => {
+   if (typeof window === "undefined" || !window.document) {
+     console.log(`bailing out of the useeffect. Going to continue to render??`)
+     return
+   }
+ },
+  <Layout>
+    <Seo title="404: Not found" />
+    <h1>404: Not Found</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </Layout>
+))
 
 export default NotFoundPage
