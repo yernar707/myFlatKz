e307f02a2672ee7396de5e4f45c0605d9eab3c1d
Yernar Yergaziyev <54137699+yernar707@users.noreply.github.com>
2022-04-26 20:05:05 +0600

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,18 @@
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /bootstrap/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+   }
+ };
+
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
