049a090171b464e462cfe67368c20059d2788a40
Yernar Yergaziyev <54137699+yernar707@users.noreply.github.com>
2022-04-26 20:26:38 +0600

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
       module: {
         rules: [
           {
-            test: /404/,
+            test: /bootstrap/,
             use: loaders.null(),
           },
         ],
