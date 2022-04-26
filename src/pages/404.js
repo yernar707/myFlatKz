bf7f5aaf39783ef41c847353906e0af220272459
Yernar Yergaziyev <54137699+yernar707@users.noreply.github.com>
2022-04-26 19:14:14 +0600

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,8 +11,8 @@ const NotFoundPage = () => (
     }
   }, [])
  <Layout>
-   <Seo title="404: Not found" />
    <div class="parent">
+      <Seo title="404: Not found" />
       <h1>404: Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
    </div>
