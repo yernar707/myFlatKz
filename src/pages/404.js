8f98ccc14cd78482b89bfb550edf0aff3a89f000
Yernar Yergaziyev <54137699+yernar707@users.noreply.github.com>
2022-04-26 20:47:51 +0600

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,18 +9,12 @@ import Seo from "../components/seo"
 const isBrowser = () => typeof window !== "undefined"
 isBrowser()
 const NotFoundPage = () => (
-  React.useEffect(() => {
-   
-  if (typeof window !== 'undefined') {
-      window.jQuery = window.$ = require('jquery');
-      require('bootstrap');
-  }
- },
-  <Layout>
+  
+  <div>
     <Seo title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  </div>
 ))
 
 export default NotFoundPage
